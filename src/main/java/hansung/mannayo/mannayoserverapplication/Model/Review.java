765d7538537055702cf560c0407c23537b7ad459
gongseunghwa <tmdghk502@naver.com>
2022-03-29 17:44:16 +0900

    Review Demo

--- src/main/java/hansung/mannayo/mannayoserverapplication/Model/Review.java
+++ src/main/java/hansung/mannayo/mannayoserverapplication/Model/Review.java
@@ -1,2 +1,51 @@
-package hansung.mannayo.mannayoserverapplication.Model;public class Review {
+package hansung.mannayo.mannayoserverapplication.Model;
+
+import lombok.*;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    @Id @GeneratedValue
+    private Integer id;
+
+    String title;
+
+    String content;
+
+    LocalDateTime writeDate;
+
+    String image;
+
+    Float starPoint;
+
+    @Column(columnDefinition = "boolean default false")
+    Boolean isDeleted;
+
+    @Column(columnDefinition = "boolean default false")
+    Boolean isModified;
+
+    LocalDateTime ModifiedDate;
+    LocalDateTime DeletedDate;
+
+    @JoinColumn()
+    @ManyToOne
+    Member member;
+
+    @ManyToOne
+    Restaurant restaurant;
+
+    @PrePersist
+    public void createAt(){
+        this.writeDate = LocalDateTime.now();
+    }
+
+
 }
--- src/test/java/hansung/mannayo/mannayoserverapplication/Model/ReviewTest.java
+++ src/test/java/hansung/mannayo/mannayoserverapplication/Model/ReviewTest.java
@@ -1,4 +1,35 @@
+package hansung.mannayo.mannayoserverapplication.Model;
+
+import org.apache.tomcat.jni.Local;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import javax.persistence.EntityManager;
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+@DataJpaTest
 class ReviewTest {
-  
+
+    @Autowired
+    EntityManager entityManager;
+
+    @Test
+    public void ReviewTest(){
+        Review review =  Review.builder()
+                .build();
+
+    entityManager.persist(review);
+
+
+        assertThat(review.getWriteDate()).isBefore(LocalDateTime.now());
+
+        System.out.println("=====================");
+        System.out.println(review.getWriteDate());
+        System.out.println("=====================");
+
+    }
+
 }
