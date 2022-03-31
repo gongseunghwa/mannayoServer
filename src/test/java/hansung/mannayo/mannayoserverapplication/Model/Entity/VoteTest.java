4f2a02060d0c4526ea2bc062838e1572d9841c65
hjk <hjkwon0814@naver.com>
2022-03-31 19:08:47 +0900

    VoteTestCreated

---
+++ src/test/java/hansung/mannayo/mannayoserverapplication/Model/Entity/VoteTest.java
@@ -0,0 +1,10 @@
+package hansung.mannayo.mannayoserverapplication.Model.Entity;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class VoteTest {
+
+}
