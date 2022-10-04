5bd1199bc914473c711f51f8b5e41adace0bc23a
hjkwon0814 <hjkwon0814@naver.com>
2022-10-04 13:54:02 +0900

    value import 수정

--- src/main/java/hansung/mannayo/mannayoserverapplication/Config/AwsS3Config.java
+++ src/main/java/hansung/mannayo/mannayoserverapplication/Config/AwsS3Config.java
@@ -5,7 +5,7 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import org.springframework.beans.factory.annotation.Value;
+import com.google.api.client.util.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
